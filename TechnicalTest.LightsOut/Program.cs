e4c387fbde78258019e453d51d6c053ee0a21a5d
Kieran <github@andersons.bz>
2019-09-24 12:13:10 -0700

    Initial project setup

---
+++ TechnicalTest.LightsOut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechnicalTest.LightsOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
